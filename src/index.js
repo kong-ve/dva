b35cca6fe13e077fcf52a75f4d0f77f2852058e1
meeshine <www.569214475@qq.com>
2019-03-28 17:12:04 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -12,29 +12,7 @@ import { LocaleProvider } from 'antd';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import 'moment/locale/zh-cn';
 import { homepage } from '../package.json';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
-import Client from 'shopify-buy';
-const httpLink = createHttpLink({ uri: 'https://mishuo-test.myshopify.com/api/graphql' })
 
-const middlewareLink = setContext(() => ({
-  headers: {
-    'X-Shopify-Storefront-Access-Token': '8f99ada3501dbc632df511e72e7220c1'
-  }
-}))
-
-const client = new ApolloClient({
-  link: middlewareLink.concat(httpLink),
-  cache: new InMemoryCache(),
-})
-
-const clients = Client.buildClient({
-  storefrontAccessToken: '8f99ada3501dbc632df511e72e7220c1',
-  domain: 'mishuo-test.myshopify.com'
-});
 // -> 初始化
 const app = dva({
   history: createHistory({
@@ -74,7 +52,7 @@ app.start('#root');
 
 // export global
 export default {
-  app,client,clients,
+  app,
   store: app._store,
   dispatch: app._store.dispatch
 };
